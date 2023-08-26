b8b6f69c98909914dbbee00520f5f0e15974f1a0
lsegecode <sanabrialucas97@gmail.com>
2023-08-25 21:55:30 -0300

    Finished day one

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'portfolio',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import CharField, URLField
+from django.db.models.fields.files import ImageField
 
-# Create your models here.
+class Project(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length = 1000)
+    image = ImageField(upload_to='portfolio/images/')
+    url = URLField(blank=True)
