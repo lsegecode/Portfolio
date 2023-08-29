0f46ac61e84e0b10260162e4046a36b81009a0cd
lsegecode <sanabrialucas97@gmail.com>
2023-08-28 22:31:35 -0300

    added blog functionality and connect different pages

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField
 import datetime
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to = 'blog/images')
     date = models.DateField(datetime.date.today)
+
+    def __str__(self) -> str:
+        return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.render_posts)
+    path('', views.render_posts),
+    path('<int:post_id>', views.post_detail)
 ]
